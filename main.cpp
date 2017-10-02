7076fa1689759c7cf6329b86046e34451a049167
Marvin Reza <marvinreza@outlook.com>
2017-10-02 23:10:50 +0200

    Now shoots ray through planes and triangles and uses inheritance

--- main.cpp
+++ main.cpp
@@ -7,27 +7,31 @@
 #include "matrix.h"
 #include "math.h"
 #include "Sphere.h"
+#include "Plane.h"
+#include "Shape.h"
+#include "Triangle.h"
 
 using byte = uint8_t;
 
 //need aspect ratio, as image might not be a square
 constexpr int height = 640, width = 840;
 constexpr float aspectRatio = width / float(height);
-constexpr int numSpheres = 5;
+constexpr int numShapes = 6;
 
 int main() {
-    Sphere sphereList[numSpheres];
-    sphereList[0]= Sphere(Vector3f(0,0,-20),4,Vector3f(1.f, 0.32f, 0.36f)); //Red sphere
-    sphereList[1]= Sphere(Vector3f(5,-1,-15),2,Vector3f(0.9f,0.76f,0.46f)); //Yellow sphere
-    sphereList[2]= Sphere(Vector3f(5,0,-25),3,Vector3f(0.65f,0.77f,0.97f)); //Light blue
-    sphereList[3]= Sphere(Vector3f(-5.5,0,-15),3,Vector3f(0.80f,0.80f,0.80f)); //Light grey
-    sphereList[4]= Sphere(Vector3f(0,-10004,-20),10000,Vector3f(0.2f,0.2f,0.2f)); //Dark grey(using a large sphere as floor for now)
+    Shape* shapes[numShapes];
+    shapes[0]= new Plane(Vector3f(0,-4,0),Vector3f(0,1,0),Vector3f(0.2,0.2,0.2)); // Dark grey floor
+    shapes[1]= new Sphere(Vector3f(0,0,-20),4,Vector3f(1,0.32,0.36)); //Red
+    shapes[2]= new Sphere(Vector3f(5,-1,-15),2,Vector3f(0.9,0.76,0.46)); //Yellow
+    shapes[3]= new Sphere(Vector3f(5,0,-25),3,Vector3f(0.65,0.77,0.97)); //Light blue 
+    shapes[4]= new Sphere(Vector3f(-5.5,0,-15),3,Vector3f(0.9,0.9,0.9)); //Light grey
+    shapes[5]= new Triangle(Vector3f(0,3,-5), Vector3f(-1.9,1.2,-5), Vector3f(1.9, 1.2, -5), Vector3f(0.8,0,1)); //Purple
 
     //img will represent the view plane
     std::vector<std::vector<Vector3f> > img(height, std::vector<Vector3f>(width));
 
     //create field of view for cam at 30 degrees(standard for video games)
-    float scale = tan(M_RAD(30.f) / 2); //30 degrees field of view
+    float scale = tan(M_RAD(90.f) / 2); //30 degrees field of view
     Vector3f rayOrigin = Vector3f(0,0,0);
 
     for(int i = 0; i < height; ++i) {
@@ -43,16 +47,15 @@ int main() {
             rayDirection = m_normalize(rayDirection);
 
             float minT = M_INFINITE, t0 = 0.0f;
-            int sphereHit = -1;
+            int shapeHit = -1;
 
-            for(int k = 0; k < numSpheres; ++k) {
-                bool doesIntersect = sphereList[k].doesIntersect(t0, rayOrigin, rayDirection);
-                //save nearest sphere
+            for(int k = 0; k < numShapes; ++k) {
+                bool doesIntersect = shapes[k]->doesIntersect(rayOrigin, rayDirection,t0);
                 if(doesIntersect && t0 < minT) {
                     minT = t0;
-                    sphereHit = k;
+                    shapeHit = k;
                 }
-                img[i][j] = sphereHit == -1 ? Vector3f(1) : sphereList[sphereHit].color;
+                img[i][j] = shapeHit == -1 ? Vector3f(1) : shapes[shapeHit]->color;
             }
         }
     }
